9a8788fd62df768f1bf23a1c8cbcf2bda88f3060
Chris Janes <gingoreilly@gmail.com>
2018-11-19 14:08:54 +0000

    add player bullets and basic bullet pool logic for the player

--- ScreamBeam/Assets/Scripts/Player/PlayerAttack.cs
+++ ScreamBeam/Assets/Scripts/Player/PlayerAttack.cs
@@ -4,13 +4,45 @@ using UnityEngine;
 
 public class PlayerAttack : MonoBehaviour {
 
+    public GameObject BulletPrefab;
+    public GameObject[] BulletPool;
+    public int MaxBullets;
+
 	// Use this for initialization
 	void Start () {
-		
+        BulletPool = new GameObject[MaxBullets];
+
+        for (int i = 0; i < MaxBullets; i++)
+        {
+            BulletPool[i] = Instantiate(BulletPrefab);
+            BulletPool[i].SetActive(false);
+        }
 	}
 	
+    private GameObject GetSpareBullet()
+    {
+        for (int i = 0; i < MaxBullets; i++)
+        {
+            if ( BulletPool[i].activeSelf == false)
+            {
+                return BulletPool[i];
+            }
+        }
+
+        return null;
+    }
+
 	// Update is called once per frame
 	void Update () {
-		
+		if ( Input.GetButtonDown("Fire1"))
+        {
+            GameObject bullet = GetSpareBullet();
+
+            if (bullet != null)
+            {
+                bullet.SetActive(true);
+                bullet.transform.position = transform.position;
+            }
+        }
 	}
 }
