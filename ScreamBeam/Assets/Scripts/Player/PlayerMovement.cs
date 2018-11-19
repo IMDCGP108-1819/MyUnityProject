802f49c125550357647895c8f332e2cd1b59fd1e
Chris Janes <gingoreilly@gmail.com>
2018-11-19 13:41:41 +0000

    add player movement and empty player attack script

---
+++ ScreamBeam/Assets/Scripts/Player/PlayerMovement.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+
+    public float HorizontalMovement;
+    public Rigidbody2D rb;
+
+    private void LateUpdate()
+    {
+        if (transform.position.x > 8.5)
+        {
+            transform.position = new Vector3(8.5f, transform.position.y);
+        }
+
+        if (transform.position.x < -8.5)
+        {
+            transform.position = new Vector3(-8.5f, transform.position.y);
+        }
+    }
+
+    void FixedUpdate () {
+        float horizontal = Input.GetAxis("Horizontal") * HorizontalMovement;
+
+        rb.velocity = new Vector2(horizontal, rb.velocity.y); 
+	}
+}
